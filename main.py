7859256a3078371e2d4f02adffdaa86fc1bd8932
Mario Valenzuela <mariovalpa@gmail.com>
2019-04-29 11:40:35 -0600

    Se leen y procesan todos los csv para entrenar el modelo

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
+import os
+
 from sklearn.model_selection import train_test_split
 
 import models.preprocessing as pp
 from models.cnn_1d import Cnn1DModel
 
-dataframe = pp.read_csv('data/6dhrt.csv')
-dataframe = pp.get_measured_carbohydrates_range_data(dataframe)
-dataframe = pp.fill_interpolable_data(dataframe)
-dataframe = pp.fill_non_interpolable_data(dataframe)
+import numpy as np
 
-x, y = pp.slide_window(dataframe)
+path = 'data/'
 
-x_normalized, _ = pp.normalize(x)
-y_normalized, _ = pp.normalize(y)
+files = os.listdir(path)
+
+x = list()
+y = list()
+
+for file in files:
+    filepath = os.path.join(path, file)
+    dataframe = pp.read_csv(filepath)
+    dataframe = pp.get_measured_carbohydrates_range_data(dataframe)
+    dataframe = pp.fill_interpolable_data(dataframe)
+    dataframe = pp.fill_non_interpolable_data(dataframe)
+    xn, yn = pp.slide_window(dataframe)
+    x.extend(xn.values)
+    y.extend(yn.values)
+
+x = np.array(x, dtype='float64')
+y = np.array(y, dtype='float64')
+
+x_normalized, x_scaler = pp.normalize(x)
+y_normalized, y_scaler = pp.normalize(y)
 
 x_train, x_test, y_train, y_test = train_test_split(x_normalized, y_normalized, test_size=0.30, random_state=1337)
 
