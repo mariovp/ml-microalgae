49c3cc2a414db091c0d4ac3fc380e5f76a57ebff
Mario Valenzuela <mariovalpa@gmail.com>
2019-05-15 00:29:51 -0600

    Se elimina código viejo de ann y knn

--- models/ann.py
+++ models/ann.py
@@ -26,37 +26,3 @@ class AnnModel(BaseModel):
                               shuffle=1,
                               callbacks=[ea],
                               validation_split=0.1).history
-
-
-"""class DnnModel:
-
-    def __init__(self, x_train: ndarray, y_train: ndarray, x_test: ndarray, y_test: ndarray):
-        self.x_train = x_train
-        self.y_train = y_train
-        self.x_test = x_test
-        self.y_test = y_test
-
-        self.model = Sequential()
-        self.model.add(Dense(2, activation='relu'))
-        self.model.add(Dense(50, activation='relu'))
-        self.model.add(Dense(1))
-        self.model.compile(optimizer='adadelta', loss='mean_squared_error', metrics=['accuracy'])
-        print("New DNN model initialized")
-
-        self.ea = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
-
-    def fit(self):
-        history = self.model.fit(self.x_train, self.y_train, epochs=150, batch_size=64, verbose=1,
-                                 shuffle=1,
-                                 callbacks=[self.ea],
-                                 validation_split=0.1).history
-        mse = self.get_mse()
-        plot_model_loss(history, 'DNN', mse)
-
-    def get_mse(self):
-        y_predicted = self.model.predict(self.x_test)
-        return mean_squared_error(self.y_test, y_predicted)
-
-    def plot_real_vs_predicted(self):
-        y_predicted = self.model.predict(self.x_test)
-        plot_real_vs_predicted_values(self.y_test, y_predicted, 'DNN')"""
